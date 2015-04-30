e61ab9b224439d3835c1457bf810cd1b20638b1b
Roger Almeida <roger.eduardo@gmail.com>
2015-04-30 17:21:23 +1000

    Organizing the bootstrap file

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,28 +1,39 @@
 GIT_USER=`git config --global --get user.name`
-if [ -z ${GIT_USER} ]; then
+if [ -z "${GIT_USER}" ]; then
 	echo -n "Please enter your git user.name: "
 	read GIT_USER
 	git config --global user.name "${GIT_USER}"
 fi
 
 GIT_EMAIL=`git config --global --get user.email`
-if [ -z ${GIT_EMAIL} ]; then
+if [ -z "${GIT_EMAIL}" ]; then
 	echo -n "Please enter your git user.email: "
 	read GIT_EMAIL
 	git config --global user.email "${GIT_EMAIL}"
 fi
 
-echo "Installing Command-Line-Tools and Xcode 5"
-xcode-select --install
-
-if [ ! -d /opt ]; then
-	mkdir /opt
+echo
+echo
+echo "Intalling boxen in /opt"
+if [ ! -d "/opt" ]; then
+	sudo mkdir /opt
 fi
-echo "Creating link in /opt"
+if [ ! -f "/opt/boxex/bootstrap.sh" ]; then
+	echo "Creating link in /opt"
+	sudo ln -s $HOME/my-boxen /opt/boxen
+	sudo chown -R $USER  /opt/boxen
+fi
 
-cd /opt && sudo ln -s $HOME/my-boxen boxen
-sudo chown -R $USER  /opt/boxen
+echo
+echo
+XCODE_PATH=`xcode-select -p`
+if [ -z "${XCODE_PATH}" ]; then
+	echo "Installing Command-Line-Tools and Xcode 5"
+	xcode-select --install
+fi
 
+echo
+echo
 echo "Running boxen for the first time"
 /opt/boxen/script/boxen
 
