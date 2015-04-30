d20d027c5375bbadb16c14d99156c98335d7afac
rogeralmeida <roger.eduardo@gmail.com>
2015-04-30 16:43:52 +1000

    Verifying if /opt exists to create it

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,12 @@
 echo "Installing Command-Line-Tools and Xcode 5"
 xcode-select --install
 
+if [ ! -d /opt ]; then
+	mkdir /opt
+fi
 echo "Creating link in /opt"
-cd /opt && sudo ln -s $HOME/our-boxen boxen
+
+cd /opt && sudo ln -s $HOME/my-boxen boxen
 sudo chown -R $USER  /opt/boxen
 
 echo "Running boxen for the first time"
